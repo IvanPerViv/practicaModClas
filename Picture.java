69dcb71355e9e23e5b5b02b573ebd26d376d1876
ivan <ivan.ipv3@gmail.com>
2019-10-10 16:12:39 +0100

    Creacion campo verde y simulacion del sol cuando amanece

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Square camp;
 
     /**
      * Constructor for objects of class Picture
@@ -48,12 +49,22 @@ public class Picture
         roof.moveVertical(-60);
         roof.makeVisible();
 
+        camp = new Square();
+        camp.changeColor("green");
+        camp.moveHorizontal(-310);
+        camp.moveVertical(140);
+        camp.changeSize(500);
+        camp.makeVisible();
+        
         sun = new Circle();
         sun.changeColor("yellow");
-        sun.moveHorizontal(100);
-        sun.moveVertical(-40);
         sun.changeSize(80);
+        sun.moveHorizontal(100);
+        sun.moveVertical(80);
         sun.makeVisible();
+        sun.slowMoveVertical(-80);
+        
+        
     }
 
     /**
